fa13bb1b3e24998393bd2d8eb05ee98be00b6228
Juhász Bálint <JuhaszBalint@pg.local>
2025-10-08 09:50:19 +0200

    working model

--- rps.py
+++ rps.py
@@ -1,7 +1,5 @@
 print ("Szia! Köszöntünk Bálint és Marci játékában!")
 
-valasz = input ("Add meg hogy kő, papír vagy olló! \n")
-
 import random
 a = random.randint(1,3)
 
@@ -12,6 +10,8 @@ lose = 0
 x = True
 
 while x:
+    valasz = input ("Add meg hogy kő, papír vagy olló! \n")
+
     if (valasz == "kő" and a == 1) or (valasz == "papír" and a == 2) or (valasz == "olló" and a == 3):
                 print ("döntetlen")
                 tie += 1
@@ -23,11 +23,19 @@ while x:
     elif (valasz == "kő" and a==3) or (valasz == "papír" and a == 1) or (valasz == "olló" and a == 2):
         print ("Nyertél!")
         win += 1
-
+    
     print (f"Győzelmek: {win}") 
     print (f"Döntetlenek: {tie}")
     print (f"Vereségek: {lose}")
+    print (f"A te választásod: {valasz}")
+    print (f"A gép választása {a}")
+    print ("    1 = kő, 2 = papír, 3 = olló")
 
-    valasz = input("Folytassuk? igen = i nem = n \n")
-    if valasz == "n":
+    answer = input("Folytassuk? igen = i nem = n \n")
+    if answer == "n":
         x = False
+
+print ("Végleges eredmény: \n")
+print (f"Győzelmek: {win}") 
+print (f"Döntetlenek: {tie}")
+print (f"Vereségek: {lose}")
